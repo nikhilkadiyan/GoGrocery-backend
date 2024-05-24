c13fe7f0273f6a97a32bbe187e1379758855f91d
nikhilkadiyan <kadiyannikhil2@gmail.com>
2024-05-24 19:25:17 +0530

    razorpay gateway added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,105 +1,120 @@
 import orderModel from "../models/orderModel.js";
-import userModel from "../models/userModel.js"
-import Stripe from "stripe";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+import userModel from "../models/userModel.js";
+import crypto from "crypto";
+import { instance } from "../server.js";
 // Placing User Order for Frontend
 const placeOrder = async (req, res) => {
+  try {
+    const options = {
+      amount: Number(req.body.amount) * 100, // amount in the smallest currency unit
+      currency: "INR",
+    };
+    const order = await instance.orders.create(options);
 
-    try {
-        const newOrder = new orderModel({
-            userId: req.body.userId,
-            items: req.body.items,
-            amount: req.body.amount,
-            address: req.body.address,
-        })
-        await newOrder.save();
-        await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
+    const newOrder = new orderModel({
+      userId: req.body.userId,
+      items: req.body.items,
+      amount: req.body.amount,
+      address: req.body.address,
+      orderId: order.id,
+    });
+    await newOrder.save();
 
-        const line_items = req.body.items.map((item) => ({
-            price_data: {
-              currency: "inr",
-              product_data: {
-                name: item.name
-              },
-              unit_amount: item.price*100*80
-            },
-            quantity: item.quantity
-          }))
-
-        line_items.push({
-            price_data:{
-                currency:"inr",
-                product_data:{
-                    name:"Delivery Charge"
-                },
-                unit_amount: 5*80*100
-            },
-            quantity:1
-        })
-        
-          const session = await stripe.checkout.sessions.create({
-            success_url: `http://localhost:5173/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `http://localhost:5173/verify?success=false&orderId=${newOrder._id}`,
-            line_items: line_items,
-            mode: 'payment',
-          });
-      
-          res.json({success:true,session_url:session.url});
-
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: "Error" })
-    }
-}
+    res.json({ success: true, order });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 // Listing Order for Admin panel
 const listOrders = async (req, res) => {
-    try {
-        const orders = await orderModel.find({});
-        res.json({ success: true, data: orders })
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: "Error" })
-    }
-}
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 // User Orders for Frontend
 const userOrders = async (req, res) => {
-    try {
-        const orders = await orderModel.find({ userId: req.body.userId });
-        res.json({ success: true, data: orders })
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: "Error" })
-    }
-}
+  try {
+    const orders = await orderModel.find({ userId: req.body.userId });
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 const updateStatus = async (req, res) => {
-    console.log(req.body);
-    try {
-        await orderModel.findByIdAndUpdate(req.body.orderId, { status: req.body.status });
-        res.json({ success: true, message: "Status Updated" })
-    } catch (error) {
-        res.json({ success: false, message: "Error" })
-    }
-
-}
+  console.log(req.body);
+  try {
+    await orderModel.findByIdAndUpdate(req.body.orderId, {
+      status: req.body.status,
+    });
+    res.json({ success: true, message: "Status Updated" });
+  } catch (error) {
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 const verifyOrder = async (req, res) => {
-    const {orderId , success} = req.body;
-    try {
-        if (success==="true") {
-            await orderModel.findByIdAndUpdate(orderId, { payment: true });
-            res.json({ success: true, message: "Paid" })
-        }
-        else{
-            await orderModel.findByIdAndDelete(orderId)
-            res.json({ success: false, message: "Not Paid" })
-        }
-    } catch (error) {
-        res.json({ success: false, message: "Not  Verified" })
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    req.body;
+  try {
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+    const expectedSignature = crypto
+      .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
+      .update(body.toString())
+      .digest("hex");
+
+    const isAuthentic = expectedSignature === razorpay_signature;
+    const order = await orderModel.findOne({ orderId: razorpay_order_id });
+    if (isAuthentic) {
+      await orderModel.findByIdAndUpdate(order._id, {
+        payment: true,
+      });
+      await userModel.findOneAndUpdate(
+        { _id: order.userId },
+        { cartData: {} },
+        { useFindAndModify: false }
+      );
+      res.redirect(`http://localhost:5173/myorders`);
+
+      // res.redirect(`https://the-kadiyan-table-fontend.vercel.app/myorders`);
+    } else {
+      await orderModel.findByIdAndDelete(order._id);
+      res.json({ success: false, message: "Not Paid" });
     }
+  } catch (error) {
+    const order = await orderModel.findOne({ orderId: razorpay_order_id });
+    await orderModel.findByIdAndDelete(order._id);
+    res.json({ success: false, message: "Not  Verified" });
+  }
+};
+const cancelPayment = async (req, res) => {
+  const { orderId } = req.body;
+  try {
+    const result = await orderModel.deleteOne({ orderId: orderId });
+    if (result.deletedCount > 0) {
+      res
+        .status(200)
+        .send({ success: true, message: "Order deleted successfully" });
+    } else {
+      res.status(404).send({ success: false, message: "Order not found" });
+    }
+  } catch (error) {}
+};
 
-}
-
-export { placeOrder, listOrders, userOrders, updateStatus ,verifyOrder }
+export {
+  placeOrder,
+  listOrders,
+  userOrders,
+  updateStatus,
+  verifyOrder,
+  cancelPayment,
+};
--- server.js
+++ server.js
@@ -8,13 +8,20 @@ import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 import storeRouter from "./routes/storeRoute.js";
 import itemRouter from "./routes/itemRoute.js";
+import Razorpay from "razorpay";
 
 // app config
 const app = express();
 const port = process.env.PORT;
 
+export const instance = new Razorpay({
+  key_id: process.env.RAZORPAY_API_KEY,
+  key_secret: process.env.RAZORPAY_API_SECRET,
+});
+
 // middlewares
 app.use(express.json());
+app.use(express.urlencoded());
 app.use(cors());
 
 // db connection
