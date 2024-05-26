a09423da8f6de192d20ab5a0520dacfc94ba1a4d
nikhilkadiyan <kadiyannikhil2@gmail.com>
2024-05-26 20:45:28 +0530

    fixed order status

--- models/orderModel.js
+++ models/orderModel.js
@@ -5,7 +5,7 @@ const orderSchema = new mongoose.Schema({
   items: { type: Array, required: true },
   amount: { type: Number, required: true },
   address: { type: Object, required: true },
-  status: { type: String, default: "Food Processing" },
+  status: { type: String, default: "Packaging" },
   date: { type: Date, default: Date.now() },
   orderId: { type: String, required: true },
   payment: { type: Boolean, default: false },
